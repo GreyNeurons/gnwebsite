a8393bd9f99ee3df17741961b2460dd43a1acd01
Rajesh Kanade <43023475+GreyNeurons@users.noreply.github.com>
2024-10-27 02:10:33 +0530

    blog changes for adding components

--- js/blog-footer.js
+++ js/blog-footer.js
@@ -9,20 +9,22 @@ class BlogFooter extends HTMLElement {
       <link rel="stylesheet" href="/css/main.css">
 
       <!-- Your static HTML code here -->
-      <div class="columns">
+      <div class="block">
+        <div class="columns">
 
-        <div class="column has-background-dark has-text-center">
-          <div class="content  has-text-white is-family-monospace">
-            <p class = "title is-size-6 has-text-white">
-                GreyNeurons
-            </p>
-            <p class = "subtitle is-size-7 has-text-grey-light ">
-                Experience the wisdom
-            </p>
+          <div class="column has-background-dark has-text-center">
+            <div class="content  has-text-white is-family-monospace">
+              <p class = "title is-size-6 has-text-white">
+                  GreyNeurons
+              </p>
+              <p class = "subtitle is-size-7 has-text-grey-light ">
+                  Experience the wisdom
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="column has-background-dark">
-          <p class="has-text-white has-text-right is-size-7 is-family-monospace"> &copy; 2024 by GreyNeurons Consulting LLC. All rights reserved. </p>
+          <div class="column has-background-dark">
+            <div class="content has-text-white has-text-right is-size-7 is-family-monospace"> &copy; 2024 by GreyNeurons Consulting LLC. All rights reserved. </div>
+          </div>
         </div>
       </div>
     `;
--- js/blog-header.js
+++ js/blog-header.js
@@ -9,35 +9,33 @@ class BlogHeader extends HTMLElement {
       <link rel="stylesheet" href="/css/main.css">
 
       <!-- Your static HTML code here -->
-      <div class = "columns has-text-grey-light has-background-dark">
-      <div class="column">
+      <div class = "section has-text-grey-light has-background-dark">
         <div class="content has-text-white is-family-monospace">
-          <p class = "title is-size-5 has-text-white">
-            GreyNeurons
+          <p class = "title is-size-5 has-text-white  is-uppercase">
+            Grey Neurons
           </p>
           <p class = "subtitle has-text-grey-light ">
             Experience the wisdom
           </p>
         </div>
+
+        <nav class="breadcrumb" aria-label="breadcrumbs">
+          <ul>
+            
+            <li class="is-dark"><a  href="/">Home</a></li>
+            <li class="is-dark"><a  href="/articles.html">Blogs</a></li>
+            
+              <li class="is-active"><a  href="#" aria-current="page">
+                  <slot name="bread-crumb-current-page"></slot>
+                </a>
+              </li>
+            
+          </ul>
+        </nav>
+        <hr class = "gn-main"></hr>
       </div>
-      <div class=""column">
-        <div class="box has-background-dark">
-          <nav class="breadcrumb" aria-label="breadcrumbs">
-              <ul>
-                
-                <li class="is-dark"><a  href="/">Home</a></li>
-                <li class="is-dark"><a  href="/articles.html">Blogs</a></li>
-                
-                  <li class="is-active"><a  href="#" aria-current="page">
-                      <slot name="bread-crumb-current-page"></slot>
-                    </a>
-                  </li>
-                
-              </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
+ 
+    
     `;
   }
   connectedCallback() {
