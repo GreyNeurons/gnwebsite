0585ceae8661b407a428b8fddef3048e0bd47d6b
Rajesh Kanade <rkanade@gmail.com>
2025-02-06 02:32:58 +0530

    avoid donwload of css again

--- js/blog-header.js
+++ js/blog-header.js
@@ -1,44 +1,28 @@
-
 class BlogHeader extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: 'open' });
-    shadow.innerHTML = `
-
-      <link rel="stylesheet" href="/css/bulma.css">
-      <link rel="stylesheet" href="/css/main.css">
-
-      <!-- Your static HTML code here -->
-      <div class = "section has-text-grey-light has-background-dark">
+    this.innerHTML = `
+      <div class="section has-text-grey-light has-background-dark">
         <div class="content has-text-white is-family-monospace">
-          <p class = "title is-size-5 has-text-white  is-uppercase">
-            Grey Neurons
-          </p>
-          <p class = "subtitle has-text-grey-light ">
-            Experience the wisdom
-          </p>
+          <p class="title is-size-5 has-text-white is-uppercase">Grey Neurons</p>
+          <p class="subtitle has-text-grey-light">Experience the wisdom</p>
         </div>
 
         <nav class="breadcrumb" aria-label="breadcrumbs">
           <ul>
-            
-            <li class="is-dark"><a  href="/">Home</a></li>
-            <li class="is-dark"><a  href="/articles.html">Blogs</a></li>
-            
-              <li class="is-active"><a  href="#" aria-current="page">
-                  <slot name="bread-crumb-current-page"></slot>
-                </a>
-              </li>
-            
+            <li class="is-dark"><a href="/">Home</a></li>
+            <li class="is-dark"><a href="/articles.html">Blogs</a></li>
+            <li class="is-active">
+              <a href="#" aria-current="page">
+                <slot name="bread-crumb-current-page"></slot>
+              </a>
+            </li>
           </ul>
         </nav>
-        <hr class = "gn-main"></hr>
+        <hr class="gn-main">
       </div>
- 
-    
     `;
   }
-  connectedCallback() {
-  }
 }
+
 customElements.define('blog-header', BlogHeader);
