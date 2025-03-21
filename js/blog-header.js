ed95fbe8ac06347a917075c58682be38cc8e8866
Rajesh Kanade <rkanade@gmail.com>
2025-03-22 01:54:00 +0530

    cornucopia first check

--- js/blog-header.js
+++ js/blog-header.js
@@ -1,6 +1,10 @@
 class BlogHeader extends HTMLElement {
   constructor() {
     super();
+
+    const label = this.getAttribute('breadcrumb-label') || 'Blogs';
+    const url = this.getAttribute('breadcrumb-url') || '/articles.html';
+
     this.innerHTML = `
       <div class="section has-text-grey-light has-background-dark">
         <div class="content has-text-white is-family-monospace">
@@ -11,7 +15,7 @@ class BlogHeader extends HTMLElement {
         <nav class="breadcrumb" aria-label="breadcrumbs">
           <ul>
             <li class="is-dark"><a href="/">Home</a></li>
-            <li class="is-dark"><a href="/articles.html">Blogs</a></li>
+            <li class="is-dark"><a href="${url}">${label}</a></li>
             <li class="is-active">
               <a href="#" aria-current="page">
                 <slot name="bread-crumb-current-page"></slot>
