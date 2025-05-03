50b346673048b7bdfb6e5cb4eba8c86718f47fc9
Rajesh Kanade <rkanade@gmail.com>
2025-05-03 18:13:19 +0530

    Refactor no-ads-cta.js to improve CTA display logic

--- js/no-ads-cta.js
+++ js/no-ads-cta.js
@@ -6,11 +6,14 @@
     
         // 2) If either is set, don’t show the CTA at all
         if (hasGnFlag ) {
-          // Option A: just leave it empty
-          //return;
 
-          // Option B: remove the element from the DOM entirely
-          this.remove();
+          this.innerHTML = `
+          <div class="notification is-light is-size-6 has-text-centered" style="margin-bottom: 1rem;">
+             This article may <b>display ads</b> if it's more than a few weeks old..<br>
+            Prefer a cleaner, distraction-free experience? Subscribe to our newsletter
+              <iframe src="https://embeds.beehiiv.com/8fd667ce-4b02-4034-97b7-b96e88fd9afc?slim=true" data-test-id="beehiiv-embed" height="52" frameborder="0" scrolling="no" style="margin: 0; border-radius: 0px !important; background-color: transparent;"></iframe>
+          </div> 
+          `;
           return;
         }
       this.innerHTML = `
@@ -18,7 +21,7 @@
            This article may <b>display ads</b> if it's more than a few weeks old..<br>
           Prefer a cleaner, distraction-free experience? Subscribe to our newsletter
             <iframe src="https://embeds.beehiiv.com/8fd667ce-4b02-4034-97b7-b96e88fd9afc?slim=true" data-test-id="beehiiv-embed" height="52" frameborder="0" scrolling="no" style="margin: 0; border-radius: 0px !important; background-color: transparent;"></iframe>
-            </div>
+      
         </div>    
       `;
     }
