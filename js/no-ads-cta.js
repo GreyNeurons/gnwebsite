052c8424a1e46fe2276635dcfede64e87a9e0d15
Rajesh Kanade <rkanade@gmail.com>
2025-05-03 18:24:13 +0530

    Refactor NoAdsCta class to improve readability and add ad-free message

--- js/no-ads-cta.js
+++ js/no-ads-cta.js
@@ -1,32 +1,32 @@
+class NoAdsCta extends HTMLElement {
+  connectedCallback() {
+    const hasGnFlag = localStorage.getItem('gn-nl-subscribed') === 'true';
 
-  class NoAdsCta extends HTMLElement {
-      connectedCallback() {
-          // 1) Check your subscription flags
-        const hasGnFlag    = localStorage.getItem('gn-nl-subscribed') === 'true';
-    
-        // 2) If either is set, don’t show the CTA at all
-        if (hasGnFlag ) {
-
-          this.innerHTML = `
-          <div class="notification is-light is-size-6 has-text-centered" style="margin-bottom: 1rem;">
-             This article may <b>display ads</b> if it's more than a few weeks old..<br>
-            Prefer a cleaner, distraction-free experience? Subscribe to our newsletter
-              <iframe src="https://embeds.beehiiv.com/8fd667ce-4b02-4034-97b7-b96e88fd9afc?slim=true" data-test-id="beehiiv-embed" height="52" frameborder="0" scrolling="no" style="margin: 0; border-radius: 0px !important; background-color: transparent;"></iframe>
-          </div> 
-          `;
-          return;
-        }
+    if (hasGnFlag) {
+      // Show the ad-free message
+      this.innerHTML = `
+        <div class="notification is-primary is-size-6 has-text-centered" style="margin-bottom: 1rem;">
+          🎉 You’re enjoying an <strong>ad-free</strong> experience! Thank you for subscribing.
+        </div>
+      `;
+    } else {
+      // Show the newsletter subscribe CTA
       this.innerHTML = `
         <div class="notification is-light is-size-6 has-text-centered" style="margin-bottom: 1rem;">
-           This article may <b>display ads</b> if it's more than a few weeks old..<br>
-          Prefer a cleaner, distraction-free experience? Subscribe to our newsletter
-            <iframe src="https://embeds.beehiiv.com/8fd667ce-4b02-4034-97b7-b96e88fd9afc?slim=true" data-test-id="beehiiv-embed" height="52" frameborder="0" scrolling="no" style="margin: 0; border-radius: 0px !important; background-color: transparent;"></iframe>
-      
-        </div>    
+          This article may <strong>display ads</strong> if it’s more than a few weeks old.<br>
+          Prefer a cleaner, distraction-free experience? Subscribe to our newsletter:
+          <div class="beehiiv-embed-wrapper" style="margin-top: 0.5rem;">
+            <iframe
+              src="https://embeds.beehiiv.com/8fd667ce-4b02-4034-97b7-b96e88fd9afc?slim=true"
+              data-test-id="beehiiv-embed"
+              height="52" frameborder="0" scrolling="no"
+              style="margin: 0; border-radius: 0 !important; background-color: transparent;">
+            </iframe>
+          </div>
+        </div>
       `;
     }
   }
+}
 
-  customElements.define('no-ads-cta', NoAdsCta);
-
-
+customElements.define('no-ads-cta', NoAdsCta);
